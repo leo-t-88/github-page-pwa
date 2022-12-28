8ee6b0f8a6dce0792a9fbde1e1c9aa4e16947adb
Léo.t88 <84191908+leo-t-88@users.noreply.github.com>
2022-12-28 12:39:30 +0100

    Update installer.js

--- installer.js
+++ installer.js
@@ -1,3 +1,10 @@
+/*
+ *  PWA Display Test. Copyright (c) 2018-2021 Tecdrop. MIT License.
+ *  https://www.tecdrop.com
+ */
+
+/* eslint-disable no-console */
+
 let installPromptEvent;
 const installLink = document.querySelector(".install-link");
 
@@ -10,19 +17,15 @@ window.addEventListener("beforeinstallprompt", event => {
 
     // Stash the event so it can be triggered later.
     installPromptEvent = event;
-
-    // Show the (hidden-by-default) install link
-    installLink.hidden = false;
 });
 
 installLink.addEventListener("click", event => {
     console.log("Install App Link Click Event");
     event.preventDefault();
 
-    // Update the install UI to remove the install button
-    installLink.hidden = true;
-
     // Show the modal add to home screen dialog
+    installPromptEvent.prompt();
+
     // Wait for the user to respond to the prompt
     installPromptEvent.userChoice.then(choice => {
         if (choice.outcome === "accepted") {
